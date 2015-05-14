253ad0e62a01ba575792e942a7ec56e7a369432c
Jessica <jessicakoch136@icloud.com>
2015-05-13 18:55:07 -0600

    user can populate data from image

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -3,16 +3,19 @@ $(function() {
 
   // var fKey = 'FLICKR_KEY;
   // var fSecret = '[FLICKR_SECRET]';
-
+  var fUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3e4c6cc06971191d5115828a46d2283d&text=Pittsburgh&sort=relevance&privacy_filter=1&accuracy=11&place_id=DlhNrQ5TVr2oUYKU&extras=url_b%2C+url_c&format=json&nojsoncallback=1&auth_token=72157652689825796-074c103e7e4ed979&api_sig=6eab14bdf9491bd44bd2b7965d4d8eb8';
   var getPhotos = (function(){
    function cityPhotos(){
     $.ajax({
-      url: 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3e4c6cc06971191d5115828a46d2283d&text=Pittsburgh&sort=relevance&privacy_filter=1&accuracy=11&place_id=DlhNrQ5TVr2oUYKU&format=json&nojsoncallback=1&auth_token=72157652689825796-074c103e7e4ed979&api_sig=c86dd0b3ab2c2c5cff05d75482fddb76',
+      url: fUrl,
       method: 'GET',
       dataType: 'JSON',
       success: function(data){
-        console.log(data);
-        // $('.photos').append('<img src="' + 'https://www.flickr.com/photos/' + photo.owner + '/' +  photo.id + '"/>');
+        var pic = data.photos.photo[0];
+        console.log(pic);
+        var picUrl = 'https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '.jpg';
+        // var photo = data['photos']['photo'][0];
+        $('.photos').append('<img src=' + picUrl + '/>');
       }
     });
   }
@@ -22,3 +25,5 @@ $(function() {
 })();
   getPhotos.cityPhotos();
 });
+
+// img src="https://c2.staticflickr.com/8/7260/8162381555_8c257edcdd_k.jpg" width="2048" height="1841" alt="Pittsburgh"></a>
