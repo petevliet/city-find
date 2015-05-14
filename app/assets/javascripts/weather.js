0f996afe1b4229d9f200590e66307247eb45d94b
Peter Vliet <petervliet@gmail.com>
2015-05-14 17:12:20 -0600

    put cityinfo in panels

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -8,7 +8,7 @@ function weather(){
       console.log(data);
       var weather = data.forecast.simpleforecast.forecastday;
         for (var index in weather) {
-          $(".weather").append('<p>Weather Forecast:</p><p>' + weather[index]['date']['weekday'] + ' ('+weather[index]['date']['monthname']+ ' '+ weather[index]['date']['day']+ ') is ' +weather[index]['conditions'] + '. ' + 'High of ' + weather[index]['high']['fahrenheit'] + '. Low of ' + weather[index]['low']['fahrenheit'] + '. ' );
+          $(".weather").append('<p>' + weather[index]['date']['weekday'] + ' ('+weather[index]['date']['monthname']+ ' '+ weather[index]['date']['day']+ ') is ' +weather[index]['conditions'] + '. ' + 'High of ' + weather[index]['high']['fahrenheit'] + '. Low of ' + weather[index]['low']['fahrenheit'] + '. ' );
           $(".icon").append('<img src=http://icons.wxug.com/i/c/k/' + (weather[index]['icon']) + '.gif>' + '</p>');
         }
 
