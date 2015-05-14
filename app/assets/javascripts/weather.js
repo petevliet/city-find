43f4b8dd8f67f58112de5224c2bd65b051f179b5
Hayley Blackstock <hayleyblackstock@gmail.com>
2015-05-14 15:38:39 -0600

    update weather

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -3,17 +3,14 @@ function weather(){
     var city = $('input').val();
     var state = $('select').val();
     $.ajax({
-      url: "http://api.wunderground.com/api/0801730a2ab7fc1d/forecast/q/"+state+"/"+city+".json"
+      url: "http://api.wunderground.com/api/8fb6aeab97ba391c/forecast/q/"+state+"/"+city+".json"
     }).done(function(data) {
       console.log(data);
-      if (data.simpleforecast == undefined)
-        alert('Please enter a valid City and State!');
-        else
-
       var weather = data.forecast.simpleforecast.forecastday;
         for (var index in weather) {
           $(".weather").append('<p>Weather Forecast:</p><p>' + weather[index]['date']['weekday'] + ' ('+weather[index]['date']['monthname']+ ' '+ weather[index]['date']['day']+ ') is ' +weather[index]['conditions'] + '. ' + 'High of ' + weather[index]['high']['fahrenheit'] + '. Low of ' + weather[index]['low']['fahrenheit'] + '. ' );
           $(".icon").append('<img src=http://icons.wxug.com/i/c/k/' + (weather[index]['icon']) + '.gif>' + '</p>');
         }
+      
     });
 }
